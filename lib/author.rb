3c2ed5c2febc7049fa7f089199103c8f53ea23e9
Jouse then <jthen93@gmail.com>
2021-02-27 02:10:13 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author
+  attr_accessor :name
+  
+  @@posts = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def posts 
+    @@posts
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(post_title)
+    new_post = Post.new(post_title)
+    new_post.author = self
+  end
+  
+  def self.post_count
+    arr = Post.all.select{|post| post.author.name == self.name}
+    return arr.count
+  end
+end 
